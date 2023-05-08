5f16fa919624ba023b9a7b147203c9856565da22
Carlos Gómez Linares <126705440+Oaks93@users.noreply.github.com>
2023-05-08 11:49:16 +0200

    ejercicio 2

---
+++ Javascript/openbootcamp/Ejercicio2/ejercicio2.js
@@ -0,0 +1,15 @@
+const me = {
+    nombre: "carlos",
+    edad: 30,
+    developer: false,
+    birthday: new Date(1993, 2, 16),
+    book: {
+        titulo: "La Tercera Puerta",
+        autor: "Alex Bamayan",
+        fecha: new Date(2018, 10),
+        link: new URL("https://www.google.es/books/edition/La_tercera_puerta/RClxDwAAQBAJ?hl=es&gbpv=1&printsec=frontcover")
+
+    }
+
+
+}
