37c26b3d3740c1e06a998d0f05e7321604e6b855
Carlos Gómez Linares <16gcarlos.93@gmail.com>
2023-05-24 16:31:49 +0200

    ex29

---
+++ Javascript/zlessons/json.js
@@ -0,0 +1,21 @@
+// console.log("Start");
+
+// setTimeout(()=>{
+//     console.log(intervalId)
+//     },6000)
+
+// console.log("end");
+
+// let count = 0
+
+// let intervalId = setInterval(() => {
+//     console.log("Carlos");
+//     count++
+//     if (count == 5) {
+//         clearInterval(intervalId)
+//     }
+    
+// }, 1000);
+
+// console.log(intervalId);
+console.error("smth went wrong")
