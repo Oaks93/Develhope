dbb2fc87b2329112cfe5bae178bdcc1dd4e84967
Carlos Gómez Linares <16gcarlos.93@gmail.com>
2023-06-27 21:02:15 +0200

    exercise1

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import Hello from "./components/Hello";
+import Message from "./components/Message";
+
+function App() {
+  return (
+   <> <Hello/>
+    <Message/>
+    </>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Message.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Message() {
+  return (
+    <div>It´s time to learn React!!!</div>
+  )
+}
