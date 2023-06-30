3b2267fc5760651e0e3415750b8fb3e8b190401a
Carlos Gómez Linares <16gcarlos.93@gmail.com>
2023-06-30 12:54:48 +0200

    exercise 5.0.1

--- my-app/src/components/WelcomewithJSX.jsx
+++ my-app/src/components/WelcomewithJSX.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function WelcomewithJSX(props) {
   return (
-    <p>Welcome, <span style={{fontWeight: "bold"}}> {props.name}</span>!</p>
+    <p>Welcome, {props.name}!</p>
   );
 }
