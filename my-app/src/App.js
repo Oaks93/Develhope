690b5c0cdb2f101c00eace898e1778ef59c12d3e
Carlos Gómez Linares <16gcarlos.93@gmail.com>
2023-06-30 12:19:22 +0200

    exercise 2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,13 @@
-import Hello from "./components/Hello";
-import Message from "./components/Message";
+// import Hello from "./components/Hello";
+// import Message from "./components/Message";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
-    <>
-      <Hello />
-      <Message />
-    </>
+    <>  
+     <Welcome name="Carlos"/>
+     </>
+
   );
 }
 
